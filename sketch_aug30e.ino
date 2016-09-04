ffc2dda8951edaa8064bc6b6cc3b00756a2d25c2
Habil Rinaldo Zulkifli <rinaldo_habil@yahoo.com>
2016-09-04 16:10:28 +0700

    Update sketch_aug30e.ino
    kami ganti high-lownya

--- sketch_aug30e.ino
+++ sketch_aug30e.ino
@@ -1,19 +1,19 @@
 void setup() {
   pinMode (A0, INPUT);
-  digitalWrite (A0, HIGH);
+  digitalWrite (A0, LOW);
   pinMode(11, OUTPUT);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  if (digitalRead(A0)==LOW)
+  if (digitalRead(A0)==HIGH)
   {
-    digitalWrite(11, HIGH);
+    digitalWrite(11, LOW);
   }
   else
   {
-    digitalWrite(11, LOW);
+    digitalWrite(11, HIGH);
   }
   // put your main code here, to run repeatedly:
 
