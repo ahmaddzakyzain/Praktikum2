bb1acb3c68fc7bfd68ab1851101e86d16571d42b
Habil Rinaldo Zulkifli <rinaldo_habil@yahoo.com>
2016-09-04 16:06:03 +0700

    Update sketch_aug30d.ino
    udah fix yaaa ;)

--- sketch_aug30d.ino
+++ sketch_aug30d.ino
@@ -5,9 +5,9 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(11, HIGH);
+  digitalWrite(12, HIGH);
   delay(500);
-  digitalWrite(11, LOW);
+  digitalWrite(12, LOW);
   delay (500);
   // put your main code here, to run repeatedly:
 
